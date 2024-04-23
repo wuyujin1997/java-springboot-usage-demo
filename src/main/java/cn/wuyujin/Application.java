57f8f53940abea3bdc1abd45ee8fee79d9805dd8
yujin-mac <wuyujin1997@outlook.com>
2024-04-23 21:23:19 +0800

    新增依赖： `spring-boot-starter` `spring-boot-starter-test`

--- src/main/java/cn/wuyujin/Application.java
+++
@@ -1,7 +0,0 @@
-package cn.wuyujin;
-
-public class Application {
-    public static void main(String[] args) {
-        System.out.println(123);
-    }
-}
---
+++ src/test/java/cn/wuyujin/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.wuyujin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("SpringBoot unit test running.");
+    }
+
+}
